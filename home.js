85ccabbee5c6c788ebfa024849a826aa12e51999
Celina Zander <zander.celina@gmail.com>
2019-04-11 10:30:21 +0200

    start

--- home.js
+++
@@ -1,4 +0,0 @@
-    <ons-card onclick="fn.pushPage({'id': 'pullHook.html', 'title': 'PullHook'})">
-      <div class="title">Pull Hook</div>
-      <div class="content">Simple "pull to refresh" functionality to update data.</div>
-    </ons-card>
